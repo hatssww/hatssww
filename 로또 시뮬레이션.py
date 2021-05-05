3b161fa28309621de134470b23734ae57405728c
hatssww <83650060+hatssww@users.noreply.github.com>
2021-05-05 11:09:34 +0900

    Update 로또 시뮬레이션.py
    파라미터 n값을 이용, 중복값이 추가되지 않도록 수정

--- 로또 시뮬레이션.py
+++ 로또 시뮬레이션.py
@@ -2,11 +2,11 @@ import random
 
 # 번호 뽑기 함수
 def generate_numbers(n):
-    random_list =[]
-    i = 1
-    while i <= 6:
-        random_list.append(random.randint(1,45))
-        i += 1
+    random_list = []
+    while len(random_list) < n:
+        num = random.randint(1,45)
+        if num not in random_list:
+            random_list.append(num)
     return random_list
 
 print(generate_numbers(6))
