4c1b08d74a01e746e45299e42c13c1070bca9a27
hatssww <83650060+hatssww@users.noreply.github.com>
2021-05-05 13:54:37 +0900

    Update 숫자 야구 게임.py
    유저에게 3개의 숫자 입력 받기

--- 숫자 야구 게임.py
+++ 숫자 야구 게임.py
@@ -11,4 +11,22 @@ def generate_numbers():
     print("0과 9 사이의 서로 다른 숫자 3개를 랜덤한 순서로 뽑았습니다.\n")
     return numbers
 
-print(generate_numbers())
+
+# 유저에게 3개의 숫자 입력받기
+def take_guess():
+    new_guess = []
+    print("숫자 3개를 하나씩 차례대로 입력하세요.")
+    i = 1
+    while i < 4:
+        user_num = int(input(f"{i}번째 숫자를 입력하세요: "))
+        if 0 <= user_num <= 9 and user_num not in new_guess:
+            new_guess.append(user_num)
+            i += 1        
+        elif user_num in new_guess:
+            print("중복되는 숫자입니다. 다시 입력하세요.")
+        else:
+            print("범위를 벗어나는 숫자입니다. 다시 입력하세요.")
+    
+    return new_guess
+
+print(take_guess())
