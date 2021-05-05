e81c7f6195cbba6e5053d2b70838764b963cfb66
hatssww <83650060+hatssww@users.noreply.github.com>
2021-05-05 11:31:57 +0900

    Update 로또 시뮬레이션.py
    겹치는 번호 개수 세는 함수 추가

--- 로또 시뮬레이션.py
+++ 로또 시뮬레이션.py
@@ -9,12 +9,17 @@ def generate_numbers(n):
             random_list.append(num)
     return random_list
 
-print(generate_numbers(6))
-
 
 # 당첨 번호 뽑기
 def draw_winning_numbers():
     winning_numbers = generate_numbers(7)
     return sorted(winning_numbers[:6]) + winning_numbers[6:]
 
-print(draw_winning_numbers())
+
+# 겹치는 번호 개수
+def count_matching_numbers(list_1, list_2):
+    s1 = set(list_1)
+    s2 = set(list_2)
+    return len(s1 & s2)
+
+print(count_matching_numbers([2, 7, 11, 14, 25, 40], [2, 11, 13, 14, 30, 35]))
