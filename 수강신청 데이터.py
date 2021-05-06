09e7653e5ebe3416b0881c62817aa75d1cc55bd1
hatssww <83650060+hatssww@users.noreply.github.com>
2021-05-06 15:07:53 +0900

    Update 수강신청 데이터.py
    학생수에 따른 강의실 배정하기

--- 수강신청 데이터.py
+++ 수강신청 데이터.py
@@ -20,3 +20,33 @@ closed_courses= list(count[count < 5].index)
 
 for course in closed_courses:
     df.loc[df['course name'] == course, 'status'] = 'not allowed'
+
+
+# 위에서 작업한 내용 적용한 새파일 이용
+df = pd.read_csv('data/enrolment_2.csv')
+
+# 개강 가능 과목 추출
+allowed = df["status"] == "allowed"
+
+# 개강 가능 과목 당 학생수 추출
+count = df.loc[allowed, "course name"].value_counts()
+
+# 강의실 사용 조건
+auditorium = list(count[count >= 80].index)
+large_room = list(count[(count < 80) & (count >= 40)].index)
+medium_room = list(count[(count < 40) & (count >= 15)].index)
+small_room = list(count[(count < 15) & (count >= 5)].index)
+
+# 폐강 과목 강의실 사용 불가 지정
+not_assigned = df["status"] == "not allowed"
+df.loc[not_assigned, "room assignment"] = "not assigned"
+
+# 강의실 지정
+for course in auditorium:
+    df.loc[(df["course name"] == course) & allowed, "room assignment"] = "Auditorium"
+for course in large_room:
+    df.loc[(df["course name"] == course) & allowed, "room assignment"] = "Large room"
+for course in medium_room:
+    df.loc[(df["course name"] == course) & allowed, "room assignment"] = "Medium room"
+for course in small_room:
+    df.loc[(df["course name"] == course) & allowed, "room assignment"] = "Small room"
