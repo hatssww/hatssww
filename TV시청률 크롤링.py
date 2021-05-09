ece2dddc3e3b04acb02162cde11e1e5206d7bd64
hatssww <83650060+hatssww@users.noreply.github.com>
2021-05-09 18:04:44 +0900

    Revert "Update TV시청률 크롤링.py"
    This reverts commit 8f766541d82823339b7e628085f6b3d94e9372bf.

--- TV시청률 크롤링.py
+++ TV시청률 크롤링.py
@@ -1,4 +1,3 @@
-import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
@@ -18,6 +17,7 @@ for i in year:
                 rating_pages.append(soup)
             week_index += 1
         month += 1
+
 
 # 레코드를 담는 빈 리스트 생성
 records = []
