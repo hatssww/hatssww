47da1e80e8e8a09b2b8919d9e60da4671c433a96
hatssww <83650060+hatssww@users.noreply.github.com>
2021-05-10 19:23:12 +0900

    Update 선형 회귀 구현하기.py
    경사 하강에 따른 손실 비용 그래프 시각화

--- 선형 회귀 구현하기.py
+++ 선형 회귀 구현하기.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 # 벡터 x에 대해서 예측값 리턴
 def prediction(theta_0, theta_1, x):
@@ -10,13 +11,23 @@ def prediction_difference(theta_0, theta_1, x, y):
     
 # 경사 하강법 함수    
 def gradient_descent(theta_0, theta_1, x, y, iterations, alpha):
+    m = len(x)
+    cost_list = []
+    
     for _ in range(iterations):  # 정해진 번만큼 경사 하강을 한다
         error = prediction_difference(theta_0, theta_1, x, y)  # 오차 값
+        cost = (error@error) / 2*m  # 손실 함수
+        cost_list.append(cost)
+
         theta_0 = theta_0 - alpha * error.mean()
         theta_1 = theta_1 - alpha * (error * x).mean()
+
+        if _ % 10 == 0:  # 10번 경사 하강할 때마다 그래프 그리기
+            plt.scatter(house_size, house_price)
+            plt.plot(house_size, prediction(theta_0, theta_1, x), color='red')
+            plt.show()
        
-    return theta_0, theta_1
-    
+    return theta_0, theta_1, cost_list    
     
 # 입력 변수(집 크기) 초기화 (모든 집 평수 데이터를 1/10 크기로 줄임)
 house_size = np.array([0.9, 1.4, 2, 2.1, 2.6, 3.3, 3.35, 3.9, 4.4, 4.7, 5.2, 5.75, 6.7, 6.9])
@@ -29,6 +40,10 @@ theta_0 = 2.5
 theta_1 = 0
 
 # 학습률 0.1로 200번 경사 하강
-theta_0, theta_1 = gradient_descent(theta_0, theta_1, house_size, house_price, 200, 0.1)
+theta_0, theta_1, cost_list = gradient_descent(theta_0, theta_1, house_size, house_price, 200, 0.1)
 
+# 결과 theta값
 print(theta_0, theta_1)
+
+# 경사 하강에 따른 손실 비용 그래프
+print(plt.plot(cost_list))
