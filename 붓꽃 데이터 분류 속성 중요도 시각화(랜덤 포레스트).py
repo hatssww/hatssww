1488af0ea0e83ea46d342205a6df3026b80084af
hatssww <83650060+hatssww@users.noreply.github.com>
2021-05-12 11:21:16 +0900

    Create 붓꽃 데이터 분류 속성 중요도 시각화(랜덤 포레스트).py
    랜덤 포레스트 모델을 이용한 붓꽃 데이터 분류 및 결정 트리 모델의 속성 중요도 시각화

---
+++ 붓꽃 데이터 분류 속성 중요도 시각화(랜덤 포레스트).py
@@ -0,0 +1,44 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# 데이터 가져오기
+iris_data = load_iris()
+
+# 입력, 목표 변수 데이터프레임 저장
+X = pd.DataFrame(iris_data.data, columns=iris_data.feature_names)
+y = pd.DataFrame(iris_data.target, columns=['class'])
+
+# train/test 셋 만들기
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=5)
+y_train = y_train.values.ravel()
+
+# 랜덤 포레스트 모델 만들기
+model = RandomForestClassifier(n_estimators=100, max_depth=4)  # 결정트리 100개, 최대 4층
+
+# 모델 학습
+model.fit(X_train, y_train)
+
+# test set 예측값
+y_predict = model.predict(X_test)
+
+# 성능 평가
+score = model.score(X_test, y_test)
+
+
+# 속성 중요도
+importances = model.feature_importances_
+
+# 결정 트리 모델의 속성 중요도 시각화
+indices_sorted = np.argsort(importances)
+
+plt.figure()
+plt.title("Feature importances")
+plt.bar(range(len(importances)), importances[indices_sorted])
+plt.xticks(range(len(importances)), X.columns[indices_sorted], rotation=90)
+plt.show()
